0c48d1fe519754c38283f96f87d4e0325e13e54a
liusenhua <senhua.liu@gmail.com>
2018-09-11 18:51:48 +0800

    The commands to run hello world example.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# hadoop-examples
+hadoop-examples
+===============
 Some examples for learning hadoop
+
+### Build
+mvn clean package
+
+### Run
+
+1. Copy the JAR to remote machine
+```
+scp hadoop-examples-1.0-SNAPSHOT.jar root@centos-7-101:/home/hadoop/test/
+```
+
+2. Prepare input
+```
+hadoop fs -mkdir /input
+hadoop fs -put LICENSE.txt /input
+hadoop fs -ls /input
+```
+
+3. Submit job
+```
+hadoop jar hadoop-examples-1.0-SNAPSHOT.jar com.liusenhua.hadoop.WordCount /input /output
+```
+
+4. Check result
+-   Check data
+```
+hadoop fs -ls /output
+hadoop fs -cat /output/part-r-00000
+```
+-   Check jobs status: http://centos-7-101:19888/jobhistory/app
+
--- src/main/java/com/liusenhua/hadoop/FileTest.java
+++ src/main/java/com/liusenhua/hadoop/FileTest.java
@@ -15,7 +15,9 @@ import java.net.URI;
  */
 public class FileTest {
     public static void main(String[] args) throws Exception {
-        String uri = "";
+        System.setProperty("HADOOP_USER_NAME", "hadoop");
+
+        String uri = "hdfs://centos-7-101:9000";
         Configuration config = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri), config);
 
@@ -26,13 +28,13 @@ public class FileTest {
         }
 
         // 在hdfs的/user根目录下创建一个文件并写入一行文本
-        FSDataOutputStream os = fs.create(new Path("/user/test.log"));
+        FSDataOutputStream os = fs.create(new Path("/user/test/test.log"));
         os.write("Hello World!".getBytes());
         os.flush();
         os.close();
 
         //显示在hdfs的/user下指定文件的内容
-        InputStream is = fs.open(new Path("/user/test.log"));
+        InputStream is = fs.open(new Path("/user/test/test.log"));
         IOUtils.copyBytes(is, System.out, 1024, true);
     }
 }
