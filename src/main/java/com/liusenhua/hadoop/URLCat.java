eba5f121726cf55870a39ef261ba31963b4a2848
LIUSENHUA613 <liusenhua613@pingan.com.cn>
2018-09-12 18:14:51 +0800

    more examples.

---
+++ src/main/java/com/liusenhua/hadoop/URLCat.java
@@ -0,0 +1,26 @@
+package com.liusenhua.hadoop;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by LIUSENHUA613 on 2018/9/12.
+ */
+public class URLCat {
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) throws Exception {
+        InputStream in = null;
+        try {
+            in = new URL(args[0]).openStream();
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
---
+++ src/test/java/com/liusenhua/hadoop/ShowFileStatusTest.java
@@ -0,0 +1,8 @@
+package com.liusenhua.hadoop;
+
+/**
+ * Created by LIUSENHUA613 on 2018/9/12.
+ */
+public class ShowFileStatusTest {
+    //private MiniDFSCluster cluster; // use an in-progress HDFS cluster
+}
